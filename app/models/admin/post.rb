36d9112cc1bbcaeb3b677ab46ec41c6b53b06a3b
Tony Jian <tonytonyjan@gmail.com>
2014-01-03 16:55:43 +0800

    annotate

--- app/models/admin/post.rb
+++ app/models/admin/post.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)      not null
+#  content    :text             not null
+#  user_id    :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Admin::Post < Post
   # scope macros
 
--- app/models/o_auth_credential.rb
+++ app/models/o_auth_credential.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: o_auth_credentials
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  provider   :string(255)      not null
+#  uid        :string(255)      not null
+#  auth_hash  :json             not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class OAuthCredential < ActiveRecord::Base
   # scope macros
 
