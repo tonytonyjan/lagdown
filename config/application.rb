cad758d5217a4fefb8386e69fe019e182ba0120a
Tony Jian <tonytonyjan@gmail.com>
2014-01-08 14:48:25 +0800

    set default generators options

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module Lagdown
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # generators
+    config.generators.assets = false
+    config.generators.helper = false
+    config.generators.jbuilder = false
   end
 end
