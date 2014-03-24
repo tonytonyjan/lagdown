aca6903e1d4c6df568c33bfdb007eeed68c8a1dd
Tony Jian <tonytonyjan@gmail.com>
2014-03-24 22:25:54 +0800

    DRY

--- app/helpers/mathjax_helper.rb
+++ app/helpers/mathjax_helper.rb
@@ -1,7 +1,8 @@
-module MathjaxHelper  
-  def mathjax_script_tag
+module MathjaxHelper
+  def mathjax_script_tag *elements
+    js_array = elements.empty? ? '[]' : "['#{elements.join("','")}']"
     result = content_tag :script, type: 'text/x-mathjax-config' do
-      "MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});".html_safe
+      "MathJax.Hub.Config({elements: #{js_array},tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});".html_safe
     end
     result += javascript_include_tag 'mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
     result
