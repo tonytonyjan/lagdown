3593f5110307f664b256565ae5a92d063444e1ae
Tony Jian <tonytonyjan@gmail.com>
2014-03-24 19:25:45 +0800

    fix mailer

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,6 +1,6 @@
 Lagdown::Application.configure do
   Settings.action_mailer.each do |key, value|
-    config.action_mailer.send("#{key}=", value.symbolize_keys)
+    config.action_mailer.send("#{key}=", value)
   end
 end
 ActionMailer::Base.register_interceptor(SandboxMailInterceptor) if Rails.env.development?
