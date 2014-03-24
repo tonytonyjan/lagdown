59cf85d239a3310819405e18c7faf2fd6a31f08d
Tony Jian <tonytonyjan@gmail.com>
2014-03-24 10:49:03 +0800

    enhance mailer config

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,5 +1,6 @@
 Lagdown::Application.configure do
-  config.action_mailer.default_url_options = {host: Settings.host}
-  config.action_mailer.smtp_settings = Settings.smtp_settings.to_options
+  Settings.action_mailer.each do |key, value|
+    config.action_mailer.send("#{key}=", value.symbolize_keys)
+  end
 end
 ActionMailer::Base.register_interceptor(SandboxMailInterceptor) if Rails.env.development?
